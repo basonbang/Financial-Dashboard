91a20ab0091634ea1ddb4f17f6e922990ec651be
Jason <jasontang20043@gmail.com>
2023-10-29 18:46:43 -0700

    linked styles to root layout & added fonts + image to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css'; // ⭐ Good practice to import CSS to top-level component
+import { inter } from '@/app/ui/fonts'; 
+
+// Defines 'RootLayout' component. Wraps all child components in <html> and <body> tags.
 export default function RootLayout({
-  children,
+  children, // takes children prop
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode;  // TS: ensures children are valid React node; used to render child components
 }) {
-  return (
+  return ( // Where child components passed to 'RootLayout' are displayed
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiasaed`}>{children}</body> {/* Apply Inter font throughout App */}
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// Keep track of fonts used throughout application
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'],
+});
