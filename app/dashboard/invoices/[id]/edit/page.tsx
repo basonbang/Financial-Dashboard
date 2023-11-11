ccd6719f29e233a1167a0b07b90961c5a11ea8f0
Jason <jasontang20043@gmail.com>
2023-11-11 00:04:07 -0800

    Error handling for invoice routes

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,18 +1,23 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 /*
     Route: /app/dashboard/invoices/[id]/edit
 */
 
-export default async function Page({params} : {params: {id: string}}) {
+export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
+      fetchInvoiceById(id),
+      fetchCustomers(),
+    ]);
 
+    if (!invoice){
+        notFound();
+    }
+  
     return (
         <main>
             <Breadcrumbs
@@ -25,7 +30,7 @@ export default async function Page({params} : {params: {id: string}}) {
                 },
                 ]}
             />
-            <Form invoice={invoice} customers={customers} />
+            <Form invoice={invoice} customers={customers}/>
         </main>
     );
 }
